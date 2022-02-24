9c440a1ad0119cba4bdd6a7793f3613c6cb49e9c
ChetanM <mchetan.more@gmail.com>
2022-02-24 22:16:48 +0530

    cleainig database for testing, and tooltips updated for actions right side of app bar on main

--- lib/screens/myApp.dart
+++ lib/screens/myApp.dart
@@ -7,6 +7,7 @@ import 'out.dart';
 import 'pending.dart';
 import 'report.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MyApp extends StatelessWidget {
   static String id = "myappscreen";
@@ -23,13 +24,40 @@ class MyApp extends StatelessWidget {
             onPressed: () {
               Navigator.pushNamed(context, searchScreen.id);
             },
+            tooltip: "Search user tax paid details",
             icon: Icon(Icons.search),
           ),
           IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.info_outline),
+            tooltip: "Cleans database for testing purpose, like fresh DB",
+            onPressed: () async {
+              //clean database for testing purpose, with no entries and db ready.
+              List collections = ['inExtra', "inWater", "inHouse", "out"];
+              for (var col in collections) {
+                var collection = FirebaseFirestore.instance.collection(col);
+                var snapshots = await collection.get();
+                for (var doc in snapshots.docs) {
+                  await doc.reference.delete();
+                }
+              }
+              //clean mainDb2021 set houseGiven & waterGiven to false
+              var collection =
+                  FirebaseFirestore.instance.collection("mainDb2021");
+              var snapshots = await collection.get();
+              for (var doc in snapshots.docs) {
+                await doc.reference
+                    .update({"houseGiven": false, "waterGiven": false});
+              }
+
+              //clean formula
+              FirebaseFirestore.instance
+                  .collection('formula')
+                  .doc("calculation")
+                  .update({'totalIn': 0, 'totalOut': 0});
+            },
+            icon: Icon(Icons.cleaning_services),
           ),
           IconButton(
+            tooltip: "Log out",
             onPressed: () {
               _auth.signOut();
               Navigator.pop(context);
