ceba76d53dddeaf8e94df6ce6dc4e3de2466e8be
ChetanM <mchetan.more@gmail.com>
2022-03-09 02:31:40 +0530

    unnecessary headers removed

--- lib/screens/outList.dart
+++ lib/screens/outList.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:money/util.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'inList.dart';
 
 class outList extends StatelessWidget {
   String outType = "";
