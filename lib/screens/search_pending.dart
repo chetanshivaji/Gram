51f42fbee4f4cfe77ea2a89365bdcb04cf770d8a
ChetanM <mchetan.more@gmail.com>
2022-03-13 15:59:10 +0530

    search pending screen - coming back to search screen clears screen, irrespective of last search activity.

--- lib/screens/search_pending.dart
+++ lib/screens/search_pending.dart
@@ -14,12 +14,9 @@ class searchScreen extends StatefulWidget {
   _searchScreenState createState() => _searchScreenState();
 }
 
-//SingleChildScrollView objSearchList = SingleChildScrollView();
 List<DataRow> gLdr = [];
 
 String name = ""; //TODO: check if need to remove this
-//MaterialStateProperty<Color> clrButton =
-//MaterialStateProperty.all<Color>(Colors.red);
 
 class _searchScreenState extends State<searchScreen> {
   String mobile = "";
@@ -32,6 +29,15 @@ class _searchScreenState extends State<searchScreen> {
   String _name = "";
 
   final _formKey2 = GlobalKey<FormState>();
+
+  void setStateEmptyEntries() {
+    setState(
+      () {
+        name = '';
+      },
+    );
+    return;
+  }
 
   Future<List<DataRow>> _buildListPending() async {
     List<DataRow> ldataRow = [];
@@ -52,6 +58,9 @@ class _searchScreenState extends State<searchScreen> {
             if (value.exists) {
               mobileUserFound = true;
               var y = value.data();
+              if (_name == "") {
+                _name = y![keyName];
+              }
 
               ldataCell.add(
                 DataCell(
@@ -112,153 +121,139 @@ class _searchScreenState extends State<searchScreen> {
   Widget build(BuildContext context) {
     bool pressed = true;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(bLabelSearch),
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-            ),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  flex: 3,
-                  child: Form(
-                    key: _formKey2,
-                    child: TextFormField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          icon: Icon(Icons.mobile_friendly),
-                          hintText: msgEnterMobileNumber,
-                          labelText: labelMobile),
-                      // The validator receives the text that the user has entered.
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return msgEnterMobileNumber;
-                        }
-                        if (value == null || value.isEmpty) {
-                          return msgOnlyNumber;
-                        }
-                        if (value.length != 10) {
-                          return msgTenDigitNumber;
-                        }
-                        if (!isNumeric(value)) {
-                          return msgOnlyNumber;
-                        }
-                        mobile = value;
-                        return null;
-                      },
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    /*
-                    style: ButtonStyle(
-                      backgroundColor: clrButton,
-                    ),
-                    */
-                    child: Text(
-                      bLabelSubmit,
-                    ),
-                    onPressed: pressed
-                        ? () async {
-                            if (_formKey2.currentState!.validate()) {
-                              var ldr = await _buildListPending();
-                              //search DB
-                              for (var yr in items) {
-                                try {
-                                  _name = await FirebaseFirestore.instance
-                                      .collection(village + pin)
-                                      .doc(docMainDb)
-                                      .collection(docMainDb + yr)
-                                      .doc(mobile)
-                                      .get()
-                                      .then(
-                                    (value) {
-                                      var y = value.data();
-                                      if (y != null) {
-                                        //_name = y[keyName];
-                                        return y[keyName];
-                                      }
-                                      return "";
-                                    },
-                                  );
-                                  if (_name != "") break;
-                                } catch (e) {
-                                  popAlert(context, kTitleTryCatchFail,
-                                      e.toString(), getWrongIcon(50.0), 1);
-                                }
-                              }
-                              setState(
-                                () {
-                                  name = _name;
-                                  gLdr = ldr;
-                                  //clrButton = MaterialStateProperty.all<Color>(
-                                  //  Colors.yellow);
-                                },
-                              );
-                            }
-                            pressed = false;
+    return WillPopScope(
+      onWillPop: () {
+        print(
+            'Backbutton pressed (device or appbar button), do whatever you want.');
+
+        //trigger leaving and use own data
+        Navigator.pop(context, false);
+        gLdr = [];
+        //we need to return a future
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(bLabelSearch),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 3,
+                    child: Form(
+                      key: _formKey2,
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            icon: Icon(Icons.mobile_friendly),
+                            hintText: msgEnterMobileNumber,
+                            labelText: labelMobile),
+                        // The validator receives the text that the user has entered.
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return msgEnterMobileNumber;
                           }
-                        : null,
+                          if (value == null || value.isEmpty) {
+                            return msgOnlyNumber;
+                          }
+                          if (value.length != 10) {
+                            return msgTenDigitNumber;
+                          }
+                          if (!isNumeric(value)) {
+                            return msgOnlyNumber;
+                          }
+                          mobile = value;
+                          return null;
+                        },
+                      ),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text(
-                "$txtName$equals$name",
-                style: getTableHeadingTextStyle(),
+                  Expanded(
+                    child: ElevatedButton(
+                      child: Text(
+                        bLabelSubmit,
+                      ),
+                      onPressed: pressed
+                          ? () async {
+                              if (_formKey2.currentState!.validate()) {
+                                var ldr = await _buildListPending();
+                                if (ldr.isEmpty) {
+                                  setStateEmptyEntries();
+                                  gLdr = ldr;
+                                  return;
+                                }
+
+                                setState(
+                                  () {
+                                    name = _name;
+                                    gLdr = ldr;
+                                  },
+                                );
+                              }
+                              pressed = false;
+                            }
+                          : null,
+                    ),
+                  ),
+                ],
               ),
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text(
+                  "$txtName$equals$name",
+                  style: getTableHeadingTextStyle(),
+                ),
+              ),
+              Expanded(
                 child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: DataTable(
-                    headingTextStyle: getTableHeadingTextStyle(),
-                    border: TableBorder(
-                      horizontalInside: BorderSide(
-                        width: 1.5,
-                        color: Colors.black,
+                  scrollDirection: Axis.vertical,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: DataTable(
+                      headingTextStyle: getTableHeadingTextStyle(),
+                      border: TableBorder(
+                        horizontalInside: BorderSide(
+                          width: 1.5,
+                          color: Colors.black,
+                        ),
                       ),
+                      dataTextStyle: TextStyle(
+                        color: Colors.indigoAccent,
+                      ),
+                      columns: <DataColumn>[
+                        DataColumn(
+                          label: Text(
+                            tableHeadingYear,
+                            style: getStyle(actPending),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            tableHeadingHouse,
+                            style: getStyle(actPending),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            tableHeadingWater,
+                            style: getStyle(actPending),
+                          ),
+                        ),
+                      ],
+                      rows: gLdr,
                     ),
-                    dataTextStyle: TextStyle(
-                      color: Colors.indigoAccent,
-                    ),
-                    columns: <DataColumn>[
-                      DataColumn(
-                        label: Text(
-                          tableHeadingYear,
-                          style: getStyle(actPending),
-                        ),
-                      ),
-                      DataColumn(
-                        label: Text(
-                          tableHeadingHouse,
-                          style: getStyle(actPending),
-                        ),
-                      ),
-                      DataColumn(
-                        label: Text(
-                          tableHeadingWater,
-                          style: getStyle(actPending),
-                        ),
-                      ),
-                    ],
-                    rows: gLdr,
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
