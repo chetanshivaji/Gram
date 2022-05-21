d3f60ab2ed527dd1462bf417a99f2e86f2943004
chetanshivaji <mchetan.more@gmail.com>
2022-05-22 00:41:15 +0530

    Removed extra header includes

--- lib/readFontsAssests.dart
+++ lib/readFontsAssests.dart
@@ -1,14 +1,7 @@
-import 'dart:io';
 //import 'package:flutter/material.dart';  //dont user material.dart or it will mix up with pdf creation apis
 import 'package:flutter/services.dart';
-import 'package:money/api/pdf_api.dart';
-
-import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
 import 'package:pdf/widgets.dart';
-import 'package:money/constants.dart';
 import 'package:money/util.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 Future<void> readFontsFromAssets() async {
   var fontTableCellData = await rootBundle.load("assets/Poppins-Light.ttf");
